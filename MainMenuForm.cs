71f65a5effe4bc6b7fa301013a4bdc51d5b1ce18
shakehor-coder <shakehor@gmail.com>
2026-06-18 14:37:29 +0300

    Initial commit: WinForms Calculator and Validation Form homework

---
+++ MainMenuForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinFormsHomework
+{
+    public partial class MainMenuForm : Form
+    {
+        public MainMenuForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculator_Click(object sender, EventArgs e)
+        {
+            CalculatorForm calculatorForm = new CalculatorForm();
+            calculatorForm.Show();
+        }
+
+        private void btnValidation_Click(object sender, EventArgs e)
+        {
+            ValidationForm validationForm = new ValidationForm();
+            validationForm.Show();
+        }
+    }
+}
