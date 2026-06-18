ecb076356b1a77d9feb84093c6650fe14d59d9f9
shakehor-coder <shakehor@gmail.com>
2026-06-18 14:37:28 +0300

    Initial commit: WinForms Calculator and Validation Form homework

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace WinFormsHomework;
+
+static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MainMenuForm());
+    }
+}
